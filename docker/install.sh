6704815a20a69e92450f3bc8d0072657a5a37b8d
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2019-01-25 15:59:11 +0100

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -18,7 +18,7 @@ sudo add-apt-repository \
 sudo apt install docker-ce
 echo "> Docker installed"
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 echo "> Docker Compose installed"
