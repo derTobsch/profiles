5e756700961624e1db493b3310c8764998e3b2ec
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2020-03-13 09:20:01 +0100

    improve docker

--- docker/install.sh
+++ docker/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "/> Docker and K8s"
+echo "> Installing Docker..."
 
 sudo apt update
 sudo apt install \
@@ -18,10 +19,17 @@ sudo add-apt-repository \
 sudo apt install docker-ce
 echo "> Docker installed"
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+echo "> Installing Docker-Compose..."
+# get latest version
+LATEST=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/docker/compose/releases/latest)
+VERSION=$(echo $LATEST | sed 's:.*/::')
+echo ">> Latest Docker-Compose version is $VERSION"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/$VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-echo "> Docker Compose installed"
+echo "> Docker-Compose installed"
 
 sudo snap install kubectl --classic
 echo "> K8s installed"
