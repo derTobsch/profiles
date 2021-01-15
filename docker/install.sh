a49842798bad097caba5beb32b6a94e2cab5a8ea
Tobias Schneider <schneider@synyx.de>
2021-01-15 10:52:10 +0100

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -26,9 +26,7 @@ echo "> Docker installed"
 
 
 echo "> Installing Docker-Compose..."
-sudo curl -L "https://github.com/docker/compose/releases/download/latest/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+sudo apt-get install docker-compose
 echo "> Docker-Compose installed"
 
 sudo snap install kubectl --classic
