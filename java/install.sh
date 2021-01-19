8ac58c00501aac32b284f27e48a81a5c631f3073
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2021-01-19 20:32:30 +0100

    jabba will automatically add to zshrc

--- java/install.sh
+++ java/install.sh
@@ -3,6 +3,3 @@
 echo "/> Jabba"
 
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-[[ -f ${HOME}/.zshrc ]] && echo "source $DIR/.java-source" >> ${HOME}/.zshrc;
